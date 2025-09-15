2881f0a4329a103c27bc8d94fab14b1a008086c6
lazydeynn <deynnbiala@gmail.com>
2025-09-15 14:48:05 +0800

    added SW1 project folder

---
+++ SW1/DolarToPesoAndYen/DolarToPesoAndYen/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolarToPesoAndYen
+{
+    class Program
+    {
+
+        static double dollarToPeso(double dollar)
+        {
+            return dollar * 57.17;
+        }
+
+        static double dollarToYen(double dollar)
+        {
+            return dollar * 146.67;
+        }
+
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter currency in ($): ");
+            var inp = Console.ReadLine().Trim().Split(',');
+
+            List<double> dollar = new List<double>();
+
+            for(int i = 0; i < inp.Length; i++)
+            {
+                try
+                {
+                    dollar.Add(double.Parse(inp[i]));
+                }
+                catch (FormatException)
+                {
+                    continue;
+                }
+            }
+
+            if (dollar.Count == 0)
+            {
+                Console.WriteLine("Invalid input, try again.");
+            }
+            else
+            {
+                Console.WriteLine($"\n{"Dollar($)",-12}{"Peso(P)",-12}{"Yen(¥)",-12}");
+                for (int i = 0; i < dollar.Count; i++)
+                {
+                    Console.WriteLine($"{dollar[i],-12}{dollarToPeso(dollar[i]),-12}{dollarToYen(dollar[i]),-12}");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SW1/PrimeCompositeChecker/PrimeCompositeChecker/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCompositeChecker
+{
+    class Program
+    {
+        static bool IsPrime(int number)
+        {
+            if (number <= 1)
+                return false;
+
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter string: ");
+            String inp = Console.ReadLine().Trim();
+            var str = inp.Split();
+
+            List<int> num = new List<int>();
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                try
+                {
+                    num.Add(int.Parse(str[i]));
+                }
+                catch (FormatException)
+                {
+                    continue;
+                }
+            }
+
+            if(num.Count == 0) Console.WriteLine("Your input doesn't contain a number.");
+
+
+            Console.WriteLine("");
+            for(int i = 0; i < num.Count; i++)
+            {
+                if (IsPrime(num[i])){
+                    Console.WriteLine($"{i+1}. {num[i]}   Prime");
+                }
+                else
+                {
+                    Console.WriteLine($"{i+1}. {num[i]}   Composite");
+                }
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
